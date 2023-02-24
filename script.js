91c8221f89e2669ca9d92cd1ced7e5ad9c827c31
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2023-02-23 19:35:32 -0800

    script

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-// var students = ['Bob', 'Jill', 'Tom']
-// var years = [1997,2002, 23]
-// var isTrue = [true, false, false, true]
-// var name = 'Shayna'
-// var number = 30
+const date = new Date();
 
+const months =[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
 
-// // function add(num1, num2) {
-// //     console.log(num1 + num2)
-// // }
-
-// // function subtract(num1, num2) {
-// //     console.log(num1 - num2)
-// // }
-
-// // add(19, 1)
-
-// function getName2() {
-//     var name2 = 'Donnahue'
-//     console.log(name2)
-// }
-
-
-// getName2()
+document.querySelector(".date h1").innerHTML 
+= months[date.getMonth()];
